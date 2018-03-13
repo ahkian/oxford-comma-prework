a0d02140a2130d53f80f7159fa618702f31ecb3f
Daniel Brazel <dbrazel4@gmail.com>
2018-03-13 16:36:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  string = []
+  array.each_with_index do |el, idx|
+      string << el 
+  end
+  string.pop
+  string = string.join(", ")
+  string = string + ", and " + array[-1]
+  return puts string
 end
